8cb53e28114a5f5a3e99bebc02a4bcb03b2b6f91
Valerio Cietto <valerio_ciet@hotmail.it>
2015-07-21 14:21:37 +0200

    Create TastieraBase.ino

---
+++ TastieraBase.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Keyboard.begin();
+
+  delay(800);
+  Keyboard.println("ciao");
+}
+
+void loop() {
+   delay(800);
+  Keyboard.println("ciao");
+}
